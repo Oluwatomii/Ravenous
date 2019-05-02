f4e31132a9e616e1a9ec2be118e7f2a2ef6660fc
Gbotolorun Oluwatomisin <tomze141@gmail.com>
2019-05-02 08:17:03 +0100

    Ravenous
    An app created using react to search for food using the yelp api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BusinessList from '../src/BusinessList/BusinessList';
+import SearchBar from '../src/SearchBar/SearchBar';
+
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+const buisnesses = [business, business, business, business, business, business];
 
 class App extends Component {
+  searchYelp(term = "Pizza", location = "brooklyn", sortBy = "best_match"){
+    console.log(`Searching Yelp with ${term} , ${location} and ${sortBy}`);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp}/>
+        <BusinessList buisnesses={buisnesses}/>
       </div>
     );
   }
